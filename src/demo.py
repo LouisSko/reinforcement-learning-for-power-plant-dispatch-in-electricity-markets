0393b22142dac826e484e07a31f12fa34e089c3d
Alexander Grote <alexander.grote@kit.edu>
2022-04-21 11:13:48 +0200

    adding dummy tests, updating requirements and adding setup.py

---
+++ src/demo.py
@@ -0,0 +1,18 @@
+# this file can be deleted and serves demo purposes only
+
+def say_hello(name: str) -> str:
+    """
+    This function returns a string that contains a greeting for a person.
+    It accepts a name as a string variable as input.
+
+    :param name: a name as a string variable
+    :return: a string variable that contains a greeting
+    """
+
+    return f'hello {name}'
+
+
+if __name__ == '__main__':
+    
+    greeting = say_hello(name='alex')
+    print(greeting)
