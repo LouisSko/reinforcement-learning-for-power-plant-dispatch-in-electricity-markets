40cbd99c781bd9801b1e90690bf9fa8a6babe587
Louis Skowronek <ulhni@student.kit.edu>
2023-07-10 18:10:11 +0200

    updated unit tests

--- src/test_main.py
+++ src/test_main.py
@@ -4,33 +4,32 @@ import numpy as np
 from unittest.mock import Mock, patch
 from environment import market_env
 from ppo_torch import PPOAgent
+import pytest
 
-class TestMainMethods(unittest.TestCase):
-    
-    def setUp(self):
-        # Define any common resources needed for tests
-        self.state_dim = 5
-        self.price_action_dim = 3
-        self.volume_action_dim = 3
-        self.lr_actor = 0.0002
-        self.lr_critic = 0.0008
-        self.gamma = 0.99
-        self.n_epochs = 10
-        self.eps_clip = 0.22
-        self.device = torch.device('cpu')
+@pytest.fixture
+def common_resources():
+    # Define any common resources needed for tests
+    state_dim = 5
+    price_action_dim = 3
+    volume_action_dim = 3
+    lr_actor = 0.0002
+    lr_critic = 0.0008
+    gamma = 0.99
+    n_epochs = 10
+    eps_clip = 0.22
+    device = torch.device('cpu')
+    ppo_agent = PPOAgent(state_dim, price_action_dim, volume_action_dim,
+                         lr_actor, lr_critic, gamma, n_epochs, eps_clip, device)
 
-        self.ppo_agent = PPOAgent(self.state_dim, self.price_action_dim, self.volume_action_dim, 
-                                  self.lr_actor, self.lr_critic, self.gamma, self.n_epochs, self.eps_clip, self.device)
+    env = Mock()  # create a mock environment object
+    env.observation_space.shape = [state_dim]
+    env.action_space = [Mock(n=price_action_dim), Mock(n=volume_action_dim)]
 
-        self.env = Mock()  # create a mock environment object
-        self.env.observation_space.shape = [self.state_dim]
-        self.env.action_space = [Mock(n=self.price_action_dim), Mock(n=self.volume_action_dim)]
+    return ppo_agent, env, state_dim
 
-    def test_select_action(self):
-        # Test action selection
-        state = torch.rand(self.state_dim)  # Random state
-        action, _, _, _, _ = self.ppo_agent.select_action(state)
-        self.assertIsInstance(action.item(), int)  # Convert tensor to integer and check
-
-if __name__ == '__main__':
-    unittest.main()
+def test_select_action(common_resources):
+    ppo_agent, env, state_dim = common_resources
+    # Test action selection
+    state = torch.rand(state_dim)  # Random state
+    action, _, _, _, _ = ppo_agent.select_action(state)
+    assert isinstance(action.item(), int)  # Convert tensor to integer and check
