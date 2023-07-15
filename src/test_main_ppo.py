23c65a70feb8976d4daffdf7c78b2450e010d1dd
Louis Skowronek <ulhni@student.kit.edu>
2023-07-16 00:48:51 +0200

    add unit tests

--- src/test_main_ppo.py
+++ src/test_main_ppo.py
@@ -1,36 +1,64 @@
-import unittest
-import torch
+from main_ppo import rl_agent_run
+import pandas as pd
 import numpy as np
-from unittest.mock import Mock, patch
-from environment import market_env
-from ppo_torch import PPOAgent
+from unittest.mock import patch, MagicMock, mock_open
 import pytest
+import os
 
 @pytest.fixture
-def common_resources():
-    # Define any common resources needed for tests
-    state_dim = 5
-    price_action_dim = 3
-    volume_action_dim = 3
-    lr_actor = 0.0002
-    lr_critic = 0.0008
-    gamma = 0.99
-    n_epochs = 10
-    eps_clip = 0.22
-    device = torch.device('cpu')
-    ppo_agent = PPOAgent(state_dim, price_action_dim, volume_action_dim,
-                         lr_actor, lr_critic, gamma, n_epochs, eps_clip, device)
+def setup_hp_dict():
+    return {
+        'lower_bound': -20000,
+        'upper_bound': 20000,
+        'batch_size': 16,
+        'n_episodes': 1,
+        'update_timestep': 2048,
+        'n_epochs': 10,
+        'eps_clip': 0.22,
+        'gamma': 0.99,
+        'lr_actor': 0.0002,
+        'lr_critic': 0.0008
+    }
 
-    env = Mock()  # create a mock environment object
-    env.observation_space.shape = [state_dim]
-    env.action_space = [Mock(n=price_action_dim), Mock(n=volume_action_dim)]
 
-    return ppo_agent, env, state_dim
+@patch('os.makedirs')
+@patch('os.path.exists', return_value=False)
+@patch('torch.utils.tensorboard.SummaryWriter')
+@patch('pickle.dump')
+@patch('main_ppo.PPOAgent')
+@patch('main_ppo.market_env')
+@patch('main_ppo.read_processed_files')
+@patch("builtins.open", new_callable=mock_open)
 
-def test_select_action(common_resources):
-    ppo_agent, env, state_dim = common_resources
-    # Test action selection
-    state = torch.rand(state_dim)  # Random state
-    action, _, _, _, _ = ppo_agent.select_action(state)
-    assert isinstance(action, torch.Tensor), "action is not a tensor"
-    assert isinstance(action.item(), int),  "action.item() is not an integer"
+# very difficult to write a meaningfull unit test for this function (is it even possible?)
+def test_rl_agent_run(mock_open_file, mock_read_processed_files, mock_market_env, mock_PPOAgent, mock_pickle_dump,
+                      mock_summary_writer, mock_exists, mock_makedirs, setup_hp_dict):
+    # Set up mock objects
+    mock_agent = MagicMock()
+    mock_PPOAgent.return_value = mock_agent
+    mock_agent.select_action.return_value = [0, 0, 0, 0, 0]
+
+    mock_env = MagicMock()
+    mock_market_env.return_value = mock_env
+    mock_env.reset.return_value = [0, 0]
+    mock_env.step.return_value = [0, 0, 'done', 0, {}]
+    mock_env.observation_space.shape = [5]
+    mock_env.action_space = [MagicMock(n=3), MagicMock(n=3)]
+
+    dummy_df = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=list('ABCD'))
+    mock_read_processed_files.return_value = (
+    dummy_df, dummy_df, dummy_df, dummy_df, dummy_df, dummy_df, dummy_df, dummy_df, dummy_df)
+
+    # Mock the SummaryWriter methods that write to disk
+    mock_writer = MagicMock()
+    mock_summary_writer.return_value = mock_writer
+    mock_writer.add_scalar = MagicMock()
+    mock_writer.add_scalars = MagicMock()
+    mock_writer.close = MagicMock()
+
+    # Call the function
+    rl_agent_run(setup_hp_dict, device='cpu', train=True, model_checkpoint=None, tb_name='my_experiment', logging=False)
+
+    # Assert the PPOAgent's select_action are called
+    assert mock_agent.select_action.called
+
